643fd58f6037601127ae1a9af95959cb03baca80
Evgeny Kolesnikov <kolei@ya.ru>
2019-02-05 23:59:33 +0300

    Дописал лекцию, накидал задач

--- tasks/циклы/task1.py
+++ tasks/циклы/task1.py
@@ -1,2 +1,5 @@
 """Заполните список случайными числами. 
-Используйте в коде цикл for, функции range() и randint()"""
+Используйте в коде цикл for, функции range() и randint()
+
+https://github.com/kolei/OAP/blob/master/articles/t3l2.md#range
+"""
