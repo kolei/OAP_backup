c4b4f0ea3a76d2d5582e8cb00f21baf28dd447ba
Evgeny Kolesnikov <kolei@ya.ru>
2019-02-14 13:15:31 +0300

    work

--- tasks/циклы/task1.py
+++ tasks/циклы/task1.py
@@ -1,5 +1,18 @@
-"""Заполните список случайными числами. 
-Используйте в коде цикл for, функции range() и randint()
+import random
 
-https://github.com/kolei/OAP/blob/master/articles/t3l2.md#range
-"""
+arr=[random.randint(1,100) for i in range(6)]
+res=[-1,-1,None]
+print(arr)
+for i in range(len(arr)):
+    if res[0]<1:
+        res[0]=i
+    else:
+        if res[1]==-1:
+            res[1]==i
+            res[2]=arr[i-1]+arr[i]
+        else:
+            if arr[i-1]+arr[i]<res[2]:
+                res[0]=i-1
+                res[1]=i
+                res[2]=arr[i-1]+arr[i]
+print(res)
